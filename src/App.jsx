4e93942cc68ea4088022b0e4584900bd2271551b
AMIR <itsroiak@gmail.com>
2024-12-10 10:03:55 +0530

    Logo content

--- src/App.jsx
+++ src/App.jsx
@@ -1,124 +1,122 @@
 import React, { useEffect } from "react";
 import "./App.css";
 import { ScrollReveal } from "./js/scrollreveal.min.js";
-import navimg from "./assets/Nammazhvar.png";
-import logo from "./assets/Logo.png";
+import navimg from "./assets/sydney-sixers (logo).svg";
+import logo from "./assets/sydney-sixers.svg";
 import Particles from "react-tsparticles";
 import ParticleJson from "./particles.json";
 
 function App() {
-
-  const particleOptions={
-    "particles": {
-      "number": {
-        "value": 12,
-        "density": {
-          "enable": true,
-          "value_area": 640
-        }
-      },
-      "color": {
-        "value": "#df048c"
-      },
-      "shape": {
-        "type": "polygon",
-        "stroke": {
-          "width": 0,
-          "color": "#000"
+  const particleOptions = {
+    particles: {
+      number: {
+        value: 12,
+        density: {
+          enable: true,
+          value_area: 640,
         },
-        "polygon": {
-          "nb_sides": 6
+      },
+      color: {
+        value: "#df048c",
+      },
+      shape: {
+        type: "polygon",
+        stroke: {
+          width: 0,
+          color: "#000",
         },
-        "image": {
-          "src": "img/github.svg",
-          "width": 100,
-          "height": 100
-        }
+        polygon: {
+          nb_sides: 6,
+        },
+        image: {
+          src: "img/github.svg",
+          width: 100,
+          height: 100,
+        },
       },
-      "opacity": {
-        "value": 0.25,
-        "random": true,
-        "anim": {
-          "enable": false,
-          "speed": 1,
-          "opacity_min": 0.1,
-          "sync": false
-        }
+      opacity: {
+        value: 0.25,
+        random: true,
+        anim: {
+          enable: false,
+          speed: 1,
+          opacity_min: 0.1,
+          sync: false,
+        },
       },
-      "size": {
-        "value": 20,
-        "random": true,
-        "anim": {
-          "enable": true,
-          "speed": 10,
-          "size_min": 40,
-          "sync": false
-        }
+      size: {
+        value: 20,
+        random: true,
+        anim: {
+          enable: true,
+          speed: 10,
+          size_min: 40,
+          sync: false,
+        },
       },
-      "line_linked": {
-        "enable": false,
-        "distance": 200,
-        "color": "#ffffff",
-        "opacity": 1,
-        "width": 2
+      line_linked: {
+        enable: false,
+        distance: 200,
+        color: "#ffffff",
+        opacity: 1,
+        width: 2,
       },
-      "move": {
-        "enable": true,
-        "speed": 4,
-        "direction": "none",
-        "random": false,
-        "straight": false,
-        "out_mode": "out",
-        "bounce": false,
-        "attract": {
-          "enable": false,
-          "rotateX": 600,
-          "rotateY": 1200
-        }
-      }
+      move: {
+        enable: true,
+        speed: 4,
+        direction: "none",
+        random: false,
+        straight: false,
+        out_mode: "out",
+        bounce: false,
+        attract: {
+          enable: false,
+          rotateX: 600,
+          rotateY: 1200,
+        },
+      },
     },
-    "interactivity": {
-      "detect_on": "window",
-      "events": {
-        "onhover": {
-          "enable": false,
-          "mode": "repulse"
+    interactivity: {
+      detect_on: "window",
+      events: {
+        onhover: {
+          enable: false,
+          mode: "repulse",
         },
-        "onclick": {
-          "enable": false,
-          "mode": "push"
+        onclick: {
+          enable: false,
+          mode: "push",
         },
-        "resize": true
+        resize: true,
       },
-      "modes": {
-        "grab": {
-          "distance": 400,
-          "line_linked": {
-            "opacity": 1
-          }
+      modes: {
+        grab: {
+          distance: 400,
+          line_linked: {
+            opacity: 1,
+          },
         },
-        "bubble": {
-          "distance": 400,
-          "size": 40,
-          "duration": 2,
-          "opacity": 8,
-          "speed": 3
+        bubble: {
+          distance: 400,
+          size: 40,
+          duration: 2,
+          opacity: 8,
+          speed: 3,
         },
-        "repulse": {
-          "distance": 200,
-          "duration": 0.4
+        repulse: {
+          distance: 200,
+          duration: 0.4,
         },
-        "push": {
-          "particles_nb": 4
+        push: {
+          particles_nb: 4,
         },
-        "remove": {
-          "particles_nb": 2
-        }
-      }
+        remove: {
+          particles_nb: 2,
+        },
+      },
     },
-    "retina_detect": true
-  }
-
+    retina_detect: true,
+  };
 
   useEffect(() => {
     const navMenu = document.getElementById("nav-menu");
@@ -193,7 +191,7 @@ function App() {
       duration: 2000,
       delay: 200,
     });
- 
+
     sr.reveal(
       ".home__img, .section__title, .about__title, .about__list, .about__intro, .about__description, .about__quotes, .contact__content, .footer__content, .footer__copy"
     );
@@ -228,7 +226,6 @@ function App() {
     //       localStorage.setItem('theme', 'dark')
     //   }
     // };
-    
 
     // document
     //   .querySelector(".theme-switch")
@@ -236,35 +233,40 @@ function App() {
 
     // Load Particles
 
-    
     // Particles.load('particles-js', "particles.json", function () {
     //   console.log('callback - particles.js config loaded');
     // });
   }, []);
 
   const toggleTheme = () => {
-    console.log("toggle called")
+    console.log("toggle called");
     const body = document.body;
     const themeIcon = document.querySelector(".theme-switch");
     const currentTheme = body.classList.contains("dark-theme")
       ? "dark"
       : "light";
 
-      console.log(body, themeIcon,currentTheme)
-      if (currentTheme === 'dark') {
-        body.classList.remove('dark-theme');
-        body.style.setProperty('--color-background', 'var(--color-background-light)');
-        body.style.setProperty('--color-text', 'var(--color-text-light)');
-        themeIcon.classList.remove('ri-sun-fill')
-        themeIcon.classList.add('ri-moon-fill')
-        localStorage.setItem('theme', 'light')
+    console.log(body, themeIcon, currentTheme);
+    if (currentTheme === "dark") {
+      body.classList.remove("dark-theme");
+      body.style.setProperty(
+        "--color-background",
+        "var(--color-background-light)"
+      );
+      body.style.setProperty("--color-text", "var(--color-text-light)");
+      themeIcon.classList.remove("ri-sun-fill");
+      themeIcon.classList.add("ri-moon-fill");
+      localStorage.setItem("theme", "light");
     } else {
-        body.classList.add('dark-theme');
-        body.style.setProperty('--color-background', 'var(--color-background-dark)');
-        body.style.setProperty('--color-text', 'var(--color-text-dark)');
-        themeIcon.classList.remove('ri-moon-fill')
-        themeIcon.classList.add('ri-sun-fill')
-        localStorage.setItem('theme', 'dark')
+      body.classList.add("dark-theme");
+      body.style.setProperty(
+        "--color-background",
+        "var(--color-background-dark)"
+      );
+      body.style.setProperty("--color-text", "var(--color-text-dark)");
+      themeIcon.classList.remove("ri-moon-fill");
+      themeIcon.classList.add("ri-sun-fill");
+      localStorage.setItem("theme", "dark");
     }
   };
 
@@ -280,9 +282,9 @@ function App() {
                 alt="Nammazhvar_logo"
               />
               <div>
-                <p className="nav__logo__quote">"Dream of</p>
+                <p className="nav__logo__quote">"One</p>
                 <p className="nav__logo__quote">
-                  <span className="highlight">toxic-free</span> food"
+                  <span className="highlight">Six</span>at a Time"
                 </p>
               </div>
             </div>
@@ -326,11 +328,7 @@ function App() {
 
       {/* <div id="particles-js"></div> */}
 
-      <Particles
-      
-        id="particles-js"
-        options={particleOptions}
-      />
+      <Particles id="particles-js" options={particleOptions} />
 
       <main className="main">
         <section className="home section" id="home">
@@ -340,11 +338,9 @@ function App() {
             <div className="home__data">
               <h1 className="home__title">SYDNEY SIXERS</h1>
 
-              <h3 className="home__subtitle">Private Limited</h3>
+              <h3 className="home__subtitle">Play Cricket</h3>
 
-              <h2 className="home__description">
-                "Nourishment and Cherishment"
-              </h2>
+              <h2 className="home__description">"Unleash the Pink Storm!"</h2>
 
               <div className="home__button">
                 <a href="#about" className="button">
@@ -360,20 +356,20 @@ function App() {
 
         <section className="about section" id="about">
           <h2 className="section__title">About us</h2>
-          <h1 className="about__title">3 'S' of Alimenture</h1>
+          <h1 className="about__title">SYDNEY 6ers</h1>
           <div className="about__container container grid">
             <ul className="about__list">
               <li className="about__item">
                 <i className="ri-checkbox-fill"></i>
-                <p className="about__item__p">Sustenance</p>
+                <p className="about__item__p">Boundaries</p>
               </li>
               <li className="about__item">
                 <i className="ri-checkbox-fill"></i>
-                <p className="about__item__p">Sustainability</p>
+                <p className="about__item__p">Victories</p>
               </li>
               <li className="about__item">
                 <i className="ri-checkbox-fill"></i>
-                <p className="about__item__p">Self-reliant</p>
+                <p className="about__item__p">Memories</p>
               </li>
             </ul>
           </div>
@@ -382,34 +378,27 @@ function App() {
             <h4 className="about__intro">Dear All!</h4>
             <p className="about__description">
               <span className="about__highlight">
-                Alimenture Industries Private Limited
+                Welcome to the official website of the Sydney Sixers
               </span>{" "}
-              was incorporated on 29th December, 2023 in Chennai, Tamil Nadu
-              with a vision and a mission of Self-reliant Economy by achieving
-              sustainability in Food Industry through Natural Farming, why if,
-              without ploughing there will be no food, without food there will
-              be no lives hence, lets plough and save the lives.
-            </p>
-            <p className="about__description">
-              Alimenture Industries Private Limited is founded by{" "}
-              <span className="highlight-founder">Pavin Saminathan</span> and{" "}
-              <span className="highlight-founder">Magesh Mohan</span> passionate
-              and driven young entrepreneurs eager to make a positive impact in
-              Food industry and deeply committed to aiming Self-reliant economy
-              by achieving sustainability in Alimentation.
+              the home of energy, skill, and unrelenting passion for cricket. As
+              one of the most dynamic and successful teams in T20 cricket, the
+              Sixers embody the spirit of competition, teamwork, and excellence.
+              From thrilling sixes to game-changing moments, we bring cricket
+              fans closer to the action, creating unforgettable experiences both
+              on and off the field. Whether you're a lifelong supporter or new
+              to the game, join us in celebrating the sport we love and the
+              community that makes it all possible. Together, we are the Pink
+              Power of cricket!
             </p>
 
             <h1 className="about__title">Operation</h1>
             <p className="about__description">
-              Alimenture Industry is in the operation of manufacturing RTE
-              (Ready to Eat) food stuffs such as Bread, Biscuits, Barfi, cakes
-              and traditional confectionaries etc. those are all preparing
-              organically and free of Maida, White Sugar and synthetic additives
-              etc., (which are devastating our health significantly) and
-              Cooking/edible Oil from Sesame, Groundnut and Coconut which are
-              yield from Natural Farming and extracted by the traditional mode
-              of wooden pressing and producing Country sugar, Palm jaggery and
-              candies in natural way.
+              "Our cricket website operates with relentless energy, delivering
+              the thrill of the game straight to your screen. From live matches
+              to in-depth stats, we ignite the passion of cricket lovers
+              everywhere. With every update, every score, and every highlight,
+              we fuel the fire of competition and celebration, making every fan
+              feel the pulse of the pitch like never before!"
             </p>
 
             <h1 className="about__title">Scope</h1>
@@ -432,9 +421,9 @@ function App() {
 
             <div className="about__quotes__container">
               <p className="about__quotes">
-                Eventually, we for the reason, to pursue the moto of the great
-                NAMMAZHVAR, green crusader and agricultural scientist i.e.
-                “Dream of Toxic-free food”
+                We’re the Sydney Sixers, bold and true, Smashing sixes is what
+                we do. With skill, passion, and a team so grand, We rule the
+                pitch and “thrill the stand!”
               </p>
             </div>
           </div>
@@ -448,19 +437,13 @@ function App() {
                 <h3 className="contact__title">Write to us</h3>
 
                 <div className="contact__social">
-                  <a
-                    href="https://api.whatsapp.com/send?phone=918122762410&text=Hi."
-                    target="_blank"
-                  >
+                  <a href="#" target="_blank">
                     <i className="ri-whatsapp-fill"></i>
                   </a>
-                  <a href="" target="_blank">
+                  <a href="#" target="_blank">
                     <i className="ri-messenger-fill"></i>
                   </a>
-                  <a
-                    href="mailto:alimentureindustries@gmail.com"
-                    target="_blank"
-                  >
+                  <a href="#" target="_blank">
                     <i className="ri-mail-fill"></i>
                   </a>
                 </div>
@@ -468,14 +451,14 @@ function App() {
 
               <div>
                 <h3 className="contact__title">Call us</h3>
-                <address className="contact__info">+91 81227 62410</address>
+                <address className="contact__info">+91 8608651201</address>
               </div>
 
               <div>
                 <h3 className="contact__title">Find us here</h3>
                 <address className="contact__info">
-                  Regd. Office: 35, Mariamman Koil Street MGR Nagar, <br />
-                  Tharamani, Chennai - 600113
+                  Regd. Office: 62, Mac nagar Maran Garden, <br />
+                  Kattupakkam, Chennai - 600056
                 </address>
               </div>
             </div>
@@ -550,8 +533,8 @@ function App() {
         <span className="footer__copy">&#169; All Rights Reserved 2024</span>
         <span className="footer__copy">
           Made with <i className="ri-heart-fill"></i> by{" "}
-          <a href="https://github.com/ajhacker03" target="_blank">
-            @ajhacker
+          <a href="https://github.com/Amirkhan1201" target="_blank">
+            @amirkhan
           </a>
         </span>
       </footer>
