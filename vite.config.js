b66a5d61803ba614f10d53c3fb4b51582795f3db
AMIR <itsroiak@gmail.com>
2024-12-10 10:27:18 +0530

    sdfa

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base:"/homepage",
   plugins: [react()],
+  build: {
+    outDir: 'build', // Change output folder to 'build'
+  },
 })
